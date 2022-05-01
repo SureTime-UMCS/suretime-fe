a95887986bf483feadcca6a84a6e413b896b7edb
Przemysław Sałek <psalek1999@gmail.com>
2022-05-01 14:12:26 +0200

    players table

--- src/containers/pages/elements/Main.tsx
+++ src/containers/pages/elements/Main.tsx
@@ -4,8 +4,8 @@ const Main: React.FC<any> = ({ box }: any): JSX.Element | null => {
   const Box = box;
 
   return box ? (
-    <main className="bg-gray-100 h-full w-full">
-      <div className="flex h-full w-full p-5 justify-center items-center overflow-auto">
+    <main className="bg-gray-100 h-full w-full overflow-y-auto">
+      <div className="flex h-full w-full p-5 justify-center items-center overflow-x-hidden">
         <Box />
       </div>
     </main>
