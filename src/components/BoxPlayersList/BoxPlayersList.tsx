9d2fd7b7e3338afe727156b05dc78811601773cf
Przemysław Sałek <psalek1999@gmail.com>
2022-05-01 20:13:30 +0200

    button color change

--- src/components/BoxPlayersList/BoxPlayersList.tsx
+++ src/components/BoxPlayersList/BoxPlayersList.tsx
@@ -108,7 +108,7 @@ const BoxPlayersList: React.FC<any> = () => {
                           </div>
                         </div>
                       </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                      <td className="px-6 py-4 whitespace-nowrap text-right text-sm text-blue-500 font-medium">
                         <Link
                           to={`/profile/${encodeURIComponent(player?.email)}`}
                         >
